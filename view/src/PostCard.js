fe7f4d8bf6d662415c6528b06740ecaa30226d88
Your Name <dicomitis13@gmail.com>
2017-04-13 15:20:26 -0600

    styled individual bl blog post, added border and a max width

--- view/src/PostCard.js
+++ view/src/PostCard.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { Link } from 'react-router';
 
 const PostCard = (props) =>
-  <div className="">
-    <Link to={"/viewablog/" + props.id} > {props.content}</Link>
-    <p> {props.title} </p>
+  <div className="blog-post-style">
+    <Link to={"/viewablog/" + props.id} > {props.title}</Link>
+    <p> {props.content} </p>
     <Link to={"/editpost/" + props.id}className="btn btn-secondary"> Edit Post </Link>
     <button onClick={ () => props.deletePost(props.id)} className="btn btn-alarm">Delete Post</button>
   </div>
