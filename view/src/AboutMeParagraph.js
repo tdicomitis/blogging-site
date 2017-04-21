27dd99aa43bc74efbebb4e5179e810606951fe3a
Your Name <dicomitis13@gmail.com>
2017-04-21 13:13:00 -0600

    wrote an about me section and got rid of ipsum lorem text

--- view/src/AboutMeParagraph.js
+++ view/src/AboutMeParagraph.js
@@ -3,31 +3,29 @@ import React from 'react';
 const AboutMeParagraph = (props) =>
   <div className='site-wrap'>
     <p><img className="my-pic" src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRuuaYUAGXOuV73wC2gykNs_exvgQqdbJo97WfQ9bYJGnh4Q9MvNw"
-    alt="presentation" />At vero eos et accusamus et iusto odio dignissimos ducimus qui
-    blanditiis praesentium voluptatum deleniti atque corrupti quos dolores
-    et quas molestias excepturi sint occaecati cupiditate non provident,
-    similique sunt in culpa qui officia deserunt mollitia animi, id est
-    laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita
-    distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque
-    nihil impedit quo minus id quod maxime placeat facere possimus, omnis
-    voluptas assumenda est, omnis dolor repellendus. Temporibus autem
-    quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet
-    ut et voluptates repudiandae sint et molestiae non recusandae. Itaque
-    earum rerum hic tenetur a sapiente delectus, ut aut reiciendis
-    voluptatibus maiores alias consequatur aut perferendis doloribus
-    asperiores repellat. At vero eos et accusamus et iusto odio dignissimos ducimus qui
-    blanditiis praesentium voluptatum deleniti atque corrupti quos dolores
-    et quas molestias excepturi sint occaecati cupiditate non provident,
-    similique sunt in culpa qui officia deserunt mollitia animi, id est
-    laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita
-    distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque
-    nihil impedit quo minus id quod maxime placeat facere possimus, omnis
-    voluptas assumenda est, omnis dolor repellendus. Temporibus autem
-    quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet
-    ut et voluptates repudiandae sint et molestiae non recusandae. Itaque
-    earum rerum hic tenetur a sapiente delectus, ut aut reiciendis
-    voluptatibus maiores alias consequatur aut perferendis doloribus
-    asperiores repellat.</p>
+    alt="presentation" /><p>Short version: Adventurer. DayDreamer. Harry Potter Enthusiast. Aspiring Web Developer.</p>
+    <p>
+      Long version: I am currently competing an intensive 12 week coding bootcamp. The end goal?
+      Become a Full Stack Web Developer. Now, how did I get here? After college, having no clue
+      what I wanted to do, I did as anyone would, traveled Europe. Upon my return, my problem (obviously)
+      had not been solved. Then I did something I have always wanted to do, move to Boston. In Boston I
+      helped my company shape/create the role of the Recruiter. During this, I learned some invaluable
+      skills. </p>
+
+      <p>
+      Spending barley a year in Boston, I found myself missing my home state of Montana and the general
+      friendliness of the west coast and decided to move back. After working in the HR/Recruiting field, I
+      realized it was not the right path for me and started looking for other career avenues. I have always
+      been curious by technology and started to consider how that could play a role in my career. Being a
+      curious person, I began to teach myself how to code and became fascinated at the endless learning possibilities.
+      After a few weeks, I started searching for a more structured learning environment and was excited to find
+      the Big Sky Code Academy which allowed me to stay in MT and learn to code. The rest as they say, is history.</p>
+
+      <p>
+    I’m hoping to find a position working with a team where I can help solve problems and make meaningful contributions,
+    while also continuing to push myself as developer. When I am not trying to fulfill my goals as a Web Developer you can
+    find me running around in the mountains, enjoying a glass of cider with friends or reading a book. </p>
+    </p>
   </div>
 
 export default AboutMeParagraph;
